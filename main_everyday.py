9e5afc5ea4baf3801f1f6f25963442425c6be1fa
Kevin <75402062+Kev-in123@users.noreply.github.com>
2021-04-06 09:20:28 -0400

    Update main_everyday.py

--- main_everyday.py
+++ main_everyday.py
@@ -17,8 +17,8 @@ def attendance():
   except:
     emailpath=web.find_element_by_xpath('//*[@id="Email"]')
     
-emailpath.send_keys(email) # sends email
-emailpath.send_keys(Keys.ENTER) # signs you in
+  emailpath.send_keys(email) # sends email
+  emailpath.send_keys(Keys.ENTER) # signs you in
 
   time.sleep(1) # lets page load before continue running code or else error will occur
   username_textbox = web.find_element_by_xpath('//*[@id="UserName"]') # this is your student id 
