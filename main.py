685dad68b6b5245d348bb3b012afeae78bd0c1bf
Kevin <75402062+Kev-in123@users.noreply.github.com>
2021-04-09 12:01:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from selenium.webdriver.common.keys import Keys
 
 web = webdriver.Edge("C:\\msedgedriver.exe")
 web.get('https://forms.gle/Fccr1YANeRW6Usfu5') # used for form link
-
 id = 'student id' # this is your student id
 passw = 'password' # this is your password
-
-email='firstname.lastname@student.tdsb.on.ca' # this is your student email there might be a number
+firstName = 'First name' # this is your first name
+lastName = 'Last name' # this is your last name
+email=f'{firstName}.{lastName}@student.tdsb.on.ca' # this is your student email there might be a number
 
 try:
   emailpath=web.find_element_by_xpath('//*[@id="identifierId"]') # uses "try... except..." to get the input box because you might get a different sign-in page (I found this problem before)
@@ -36,11 +36,9 @@ time.sleep(3) # lets page load before continue running code or else error will o
 idnum = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input') # used to find the textbox for your student id
 idnum.send_keys(id) # this is to send you student id
 
-firstName = 'First name' # this is your first name
 first = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input') # used to find the textbox for your first name
 first.send_keys(firstName) # used to send first name
 
-lastName = 'Last name' # this is your last name
 last = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input') # used to find the textbox for your last name
 last.send_keys(lastName) # used to send last name
 
