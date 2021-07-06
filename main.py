86f23c635e46eff2ef076bfc9d68f6cf91dba728
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-06 18:50:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ emailpath.send_keys(email) # sends email
 emailpath.send_keys(Keys.ENTER) # signs you in
 
 time.sleep(1) # lets page load before continue running code or else error will occur
-username_textbox = web.find_element_by_xpath('//*[@id="UserName"]') # this is your student id 
+username_textbox = web.find_element_by_xpath('//*[@id="UserName"]')
 username_textbox.send_keys(id) # this is to send student id
 
 password_textbox = web.find_element_by_xpath('//*[@id="Password"]')
