d013abe92d6c9ff7eebe30a23a07d6b6fe93af2e
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-13 20:26:37 -0400

    Update main_everyday.py

--- main_everyday.py
+++ main_everyday.py
@@ -1,67 +1,68 @@
 import time
 import schedule
+from utils import get_data as user
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 
-# defines a function so its easier to use
+web = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
+web.get("https://forms.gle/Fccr1YANeRW6Usfu5")
+id_ = user['id']
+passw = user['password']
+firstName = user['first']
+lastName = user['last']
+email=f'{firstName}.{lastName}@student.tdsb.on.ca' 
+
 def attendance():
-  web = webdriver.Firefox(executable_path="C:\\geckodriver.exe") # used to open firefox double '\' to specify its a path
-  web.get("https://forms.gle/Fccr1YANeRW6Usfu5") # used for form link
-  id = 'student id' # this is your student id
-  passw = 'password' # this is your password
-  firstName = 'First name' # this is your first name
-  lastName = 'Last name' # this is your last name
-  email=f'{firstName}.{lastName}@student.tdsb.on.ca' # this is your student email there might be a number
 
   try:
-    emailpath=web.find_element_by_xpath('//*[@id="identifierId"]') # uses "try... except..." to get the input box because you might get a different sign-in page (I found this problem before)
+    try:
+      emailpath=web.find_element_by_xpath('//*[@id="identifierId"]')
+    except selenium.common.exceptions.NoSuchElementException:
+      time.sleep(0.1)
   except:
-    emailpath=web.find_element_by_xpath('//*[@id="Email"]')# used to send email
+    try:
+      emailpath=web.find_element_by_xpath('//*[@id="Email"]')
+    except selenium.common.exceptions.NoSuchElementException:
+      time.sleep(0.1)
+        
+  emailpath.send_keys(email)
+  emailpath.send_keys(Keys.ENTER)
 
-  emailpath.send_keys(email) # sends email
-  emailpath.send_keys(Keys.ENTER) # signs you in
+  while True:
+    try:
+      username_textbox = web.find_element_by_xpath('//*[@id="UserName"]')
+      break
+    
 
-  time.sleep(1) # lets page load before continue running code or else error will occur
-  username_textbox = web.find_element_by_xpath('//*[@id="UserName"]')
-  username_textbox.send_keys(id) # this is to send student id
+  username_textbox.send_keys(id)
 
   password_textbox = web.find_element_by_xpath('//*[@id="Password"]')
-  password_textbox.send_keys(passw) # this is to send password
-  password_textbox.send_keys(Keys.ENTER) # used to send data above
+  password_textbox.send_keys(passw)
+  password_textbox.send_keys(Keys.ENTER)
 
-  # You might ask why not find the button and click it. aw login won't let my code click the login button but the enter key works
 
-  # There is no confirm button in edge unlinke chrome which makes automation easier won't tell you which lines go find out yourself not gonna spoonfeed you people that much
+  while True:
+    try:
+      idnum = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+      break
+    except selenium.common.exceptions.NoSuchElementException:
+      time.sleep(0.1)
 
-  # the code below is determend on the form your teacher wants you to fill in but I will still explain the code below
+  idnum.send_keys(id)
 
-  time.sleep(3) # lets page load before continue running code or else error will occur 
-  idnum = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input') # used to find the textbox for your student id
-  idnum.send_keys(id) # this is to send you student id
+  first = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+  first.send_keys(firstName)
 
-  first = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input') # used to find the textbox for your first name
-  first.send_keys(firstName) # used to send first name
+  last = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+  last.send_keys(lastName)
+  RadioCohort = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[1]')
+  RadioCohort.click()
 
-  last = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input') # used to find the textbox for your last name
-  last.send_keys(lastName) # used to send last name
+  Cohort= web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[3]')
+  Cohort.click()
 
-  RadioCohort = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[1]') # used to find cohort dropdown menu
-  RadioCohort.click() # used to click and open cohort dropdown menu
+  Submit = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span')
+  Submit.click()
 
-  time.sleep(2) # lets options load before continue running code or else error will occur 
-  Cohort= web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[3]') # used to find your cohort
-  Cohort.click() # used to select your cohort
-
-  time.sleep(2) # I explained this too many times so I'll let you figure out this one 
-  Submit = web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span') # used to find submit button
-  Submit.click() # used to click submit button
-# to check if the form submission wwas successful
-  time.sleep(2)
-  try:
-    web.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
-    print("Form submitted successfully ")
-  except:
-    print("Unable to submit form")
-# to schedule code note: It uses 24 hour local (device) time and if its before 12:00 you must add a zero at the front (shown below)
 schedule.every().day.at("08:45").do(attendance)
 
